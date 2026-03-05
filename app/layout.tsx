cc4b444106b0560edb639396316f6479f6c3abc2
Albert Pardo pol <albertpardopol@MacBook-Air-de-Albert.local>
2026-03-05 23:00:00 +0100

    Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import type { Metadata } from "next";
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://albertpardopol.com"),
+  metadataBase: new URL("https://cirujanodemano.es"),
+  verification: {
+    google: "w71ooum1cgcONXdx26j6tv3Br2ViAAg_9Ojvbxk6tcQ",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
